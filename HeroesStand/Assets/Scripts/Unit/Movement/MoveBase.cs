19075a4370673c4bf2ba35f8058f309fd0009a57
LaikWQC <LaikWQC@gmail.com>
2020-06-26 23:43:24 +0300

    MoreMoveRefactoring

---
+++ HeroesStand/Assets/Scripts/Unit/Movement/MoveBase.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using UnityEngine;
+
+public class MoveBase : MonoBehaviour, IMoveable
+{
+    private Unit unit;
+
+    private void Awake()
+    {
+        unit = GetComponent<Unit>();
+    }
+
+    private Vector3 movementDirection;
+    private Vector3? movementPosition;
+
+    public Vector3 MovementDirection
+    {
+        get => movementDirection;
+        set
+        {
+            if (movementDirection != value)
+            {
+                movementDirection = value;
+                MovementDirectionChanged?.Invoke(this, value);
+            }
+        }
+    }
+
+    public Vector3 MovementDirectionOverride
+    {
+        get => movementDirection;
+        set
+        {
+            MovementDirection = value;
+            MovementPosition = null;
+        }
+    }
+
+    public Vector3? MovementPosition
+    {
+        get => movementPosition;
+        set
+        {
+            if (movementPosition != value)
+            {
+                movementPosition = value;
+                MovementPositionChanged?.Invoke(this, value);
+            }
+        }
+    }
+
+    public float Speed => unit.Speed;
+
+    public event EventHandler<Vector3> MovementDirectionChanged;
+    public event EventHandler<Vector3?> MovementPositionChanged;
+}
--- HeroesStand/Assets/Scripts/Unit/Unit.cs
+++ HeroesStand/Assets/Scripts/Unit/Unit.cs
@@ -1,17 +1,14 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Unit : MonoBehaviour, IMoveable
+[RequireComponent(typeof(MoveBase))]
+public class Unit : MonoBehaviour
 {
     [SerializeField] private UnitType type = UnitType.Enemy;
     private float currentHp;
     [SerializeField] private int maxHp = 20;
     [SerializeField] [Range(0.0f, 5.0f)] private float speed = 2.0f;
     [SerializeField] private float visionRadius = 4.0f;
-    private Vector3 movementDirection;
-    private Vector3? movementPosition;
     private float? attackRange;
     private Unit target;
 
@@ -42,42 +39,6 @@ public class Unit : MonoBehaviour, IMoveable
         Destroy(gameObject);
     }
 
-    public Vector3 MovementDirection
-    {
-        get => movementDirection;
-        set
-        {
-            if(movementDirection!=value)
-            {
-                movementDirection = value;
-                MovementDirectionChanged?.Invoke(this, value);
-            }            
-        }
-    }
-
-    public Vector3 MovementDirectionOverride
-    {
-        get => movementDirection;
-        set
-        {
-            MovementDirection = value;
-            MovementPosition = null;
-        }
-    }
-
-    public Vector3? MovementPosition
-    {
-        get => movementPosition;
-        set
-        {
-            if (movementPosition != value)
-            {
-                movementPosition = value;
-                MovementPositionChanged?.Invoke(this, value);
-            }            
-        }
-    }
-
     public Unit Target
     {
         get => target;
@@ -96,7 +57,5 @@ public class Unit : MonoBehaviour, IMoveable
     public float AttackRange => attackRange ?? 1.5f;
 
     public event EventHandler<Unit> TargetChanged;
-    public event EventHandler<Vector3> MovementDirectionChanged;
-    public event EventHandler<Vector3?> MovementPositionChanged;
     public event EventHandler Died;
 }
